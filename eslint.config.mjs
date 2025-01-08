e0466007e337cac2982eaf8a025e7ff727f5f352
ukihwa <ukihwa@gmail.com>
2025-01-08 17:03:10 +0900

    chore(config): configure eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,74 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import { fixupConfigRules } from '@eslint/compat';
 import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import prettierConfigRecommended from 'eslint-plugin-prettier/recommended';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import unusedImports from 'eslint-plugin-unused-imports';
+import { dirname } from 'path';
+import ts from 'typescript-eslint';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+const patchedConfig = fixupConfigRules([
+  ...compat.extends('next/core-web-vitals'),
+]);
+
+const config = [
+  { files: ['**/*.{js,mjs,cjs,ts,tsx}'] },
+  {
+    ignores: ['.next/*'],
+  },
+  ...patchedConfig,
+  ...ts.configs.recommended,
+  prettierConfigRecommended,
+  {
+    rules: {
+      'no-undef': 'off',
+      '@typescript-eslint/ban-ts-comment': 'off',
+      '@typescript-eslint/no-unused-expressions': [
+        'error',
+        { allowShortCircuit: true },
+      ],
+    },
+  },
+  // @see https://github.com/lydell/eslint-plugin-simple-import-sort
+  {
+    plugins: {
+      'simple-import-sort': simpleImportSort,
+    },
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+    },
+  },
+  // @see https://github.com/sweepline/eslint-plugin-unused-imports
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
 ];
 
-export default eslintConfig;
+export default config;
